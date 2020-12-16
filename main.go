ea0008a8a3211e83c346d60b7b6df1e05cb6ab62
Brandon Bro <>
2020-12-15 21:43:49 -0800

    updates with ssl verify

--- main.go
+++ main.go
@@ -59,7 +59,21 @@ func exportEnvironmentWithEnvman(keyStr, valueStr string) error {
 }
 
 func main() {
+
 	var cfg gitclone.Config
+
+	if len(cfg.HTTPUser) > 0 && len(cfg.HTTPToken) > 0 && strings.HasPrefix(cfg.RepositoryURL, "http") {
+		components := strings.SplitAfter(cfg.RepositoryURL, "://")
+		cfg.RepositoryURL = components[0] + cfg.HTTPUser + ":" + cfg.HTTPToken + "@" + components[1]
+		log.Infof("updated repo url: %s \n", cfg.RepositoryURL)
+	}
+
+	if !cfg.SSLVerify {
+		if err := run(gitCmd.Config("http.sslVerify", "false")); err != nil {
+			return fmt.Errorf("set config http.sslVerify false, error: %v", err)
+		}
+	}
+
 	if err := stepconf.Parse(&cfg); err != nil {
 		failf("Error: %s\n", err)
 	}
