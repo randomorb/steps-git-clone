0806187010806c2ad665613a8d8e50c1b555a9e8
Brandon Bro <>
2020-12-15 22:32:30 -0800

    .

--- gitclone/gitclone.go
+++ gitclone/gitclone.go
@@ -71,6 +71,19 @@ func getMaxEnvLength() (int, error) {
 
 // Execute is the entry point of the git clone process
 func Execute(cfg Config) *step.Error {
+
+	if len(cfg.HTTPUser) > 0 && len(cfg.HTTPToken) > 0 && strings.HasPrefix(cfg.RepositoryURL, "http") {
+		components := strings.SplitAfter(cfg.RepositoryURL, "://")
+		cfg.RepositoryURL = components[0] + cfg.HTTPUser + ":" + cfg.HTTPToken + "@" + components[1]
+		log.Infof("updated repo url: %s \n", cfg.RepositoryURL)
+	}
+
+	if !cfg.SSLVerify {
+		if err := run(gitCmd.Config("http.sslVerify", "false")); err != nil {
+			return fmt.Errorf("set config http.sslVerify false, error: %v", err)
+		}
+	}
+
 	maxEnvLength, err := getMaxEnvLength()
 	if err != nil {
 		return newStepError(
--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"strings"
 
 	"github.com/bitrise-io/go-steputils/stepconf"
 	"github.com/bitrise-io/go-utils/log"
@@ -21,18 +19,6 @@ func main() {
 		failf("Error: %s\n", err)
 	}
 	stepconf.Print(cfg)
-
-	if len(cfg.HTTPUser) > 0 && len(cfg.HTTPToken) > 0 && strings.HasPrefix(cfg.RepositoryURL, "http") {
-		components := strings.SplitAfter(cfg.RepositoryURL, "://")
-		cfg.RepositoryURL = components[0] + cfg.HTTPUser + ":" + cfg.HTTPToken + "@" + components[1]
-		log.Infof("updated repo url: %s \n", cfg.RepositoryURL)
-	}
-
-	if !cfg.SSLVerify {
-		if err := run(gitCmd.Config("http.sslVerify", "false")); err != nil {
-			return fmt.Errorf("set config http.sslVerify false, error: %v", err)
-		}
-	}
 
 	if err := gitclone.Execute(cfg); err != nil {
 		failf("ERROR: %v", err)
