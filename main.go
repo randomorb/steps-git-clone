59ec500a5fbb428a252ce937dc21554fa5607e65
Brandon Bro <>
2020-12-15 22:01:50 -0800

    .

--- main.go
+++ main.go
@@ -15,28 +15,13 @@ func failf(format string, v ...interface{}) {
 	os.Exit(1)
 }
 
-func printLogAndExportEnv(gitCmd git.Git, format, env string) error {
-	l, err := output(gitCmd.Log(format))
-	if err != nil {
-		return err
-	}
-
-	log.Printf("=> %s\n   value: %s\n", env, l)
-	if err := exportEnvironmentWithEnvman(env, l); err != nil {
-		return fmt.Errorf("envman export, error: %v", err)
-	}
-	return nil
-}
-
-func exportEnvironmentWithEnvman(keyStr, valueStr string) error {
-	cmd := command.New("envman", "add", "--key", keyStr)
-	cmd.SetStdin(strings.NewReader(valueStr))
-	return cmd.Run()
-}
-
 func main() {
-
 	var cfg gitclone.Config
+
+	if err := stepconf.Parse(&cfg); err != nil {
+		failf("Error: %s\n", err)
+	}
+	stepconf.Print(cfg)
 
 	if len(cfg.HTTPUser) > 0 && len(cfg.HTTPToken) > 0 && strings.HasPrefix(cfg.RepositoryURL, "http") {
 		components := strings.SplitAfter(cfg.RepositoryURL, "://")
@@ -49,11 +34,6 @@ func main() {
 			return fmt.Errorf("set config http.sslVerify false, error: %v", err)
 		}
 	}
-
-	if err := stepconf.Parse(&cfg); err != nil {
-		failf("Error: %s\n", err)
-	}
-	stepconf.Print(cfg)
 
 	if err := gitclone.Execute(cfg); err != nil {
 		failf("ERROR: %v", err)
